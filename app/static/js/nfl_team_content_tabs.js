dd863b8e9efe3b82734406abaf0ecc78075c99d7
Jeff Reiher <jreiher2003@yahoo.com>
2016-09-16 21:32:25 -0400

    Add jquery nested tabs for team page

--- app/static/js/nfl_team_content_tabs.js
+++ app/static/js/nfl_team_content_tabs.js
@@ -1,19 +1,4 @@
-$(document).ready(function(){
-    $(".nav-tabs > li > a").click(function(event){
-        event.preventDefault(); // stop browser to take action for clicked anchor
-        //gets displaying tab content
-        var active_tab_selector = $(".nav-tabs > li.active > a").attr("href");
-        //find active nav and remove 'active' class
-        var active_nav = $(".nav-tabs > li.active");
-        active_nav.removeClass("active");
-        // add active class to selected tab
-        $(this).parents("li").addClass("active");
-        //hide displaying tab content
-        $(active_tab_selector).removeClass("active");
-        $(active_tab_selector).addClass("hide");
-        // show target tab content 
-        var target_tab_selector = $(this).attr("href");
-        $(target_tab_selector).removeClass("hide");
-        $(target_tab_selector).addClass("active");
-    });
+$("ul.nav-tabs a").click(function (e) {
+  e.preventDefault();  
+    $(this).tab('show');
 });
