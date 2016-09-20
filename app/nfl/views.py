5c07d5cf7b5f5955631f7a29c62cf3def29723b1
Jeff Reiher <jreiher2003@yahoo.com>
2016-09-19 21:32:06 -0400

    Add run.sh nfl_confirm template and fooled around with ajax request.

--- app/nfl/views.py
+++ app/nfl/views.py
@@ -2,7 +2,7 @@ import json
 import datetime
 from app import app, db
 from forms import CreateNflBet
-from flask import Blueprint, render_template, url_for, request
+from flask import Blueprint, render_template, url_for, request, redirect,flash
 from slugify import slugify
 from .utils import team_rush_avg, team_pass_avg, \
 opp_team_rush_avg, opp_team_pass_avg, team_off_avg, \
@@ -58,9 +58,22 @@ def nfl_create_broad():
     return "create reg"
 
 @nfl_blueprint.route("/nfl/board/create/<path:game_key>/", methods=["GET","POST"])
+# @nfl_blueprint.route("/nfl/board/create/", methods=["GET","POST"])
 def nfl_create_bet(game_key):
     all_teams = nflteam
     form = CreateNflBet()
+    # if request.method == "POST":
+    # game_key = request.form["game_key"]
+    # over_under = request.form["over_under"]
+    # home_team = request.form["home_team"]
+    # home_ml = request.form["home_ml"]
+    # home_ps = request.form["home_ps"]
+    # away_team = request.form["away_team"]
+    # away_ml = request.form["away_ml"]
+    # away_ps = request.form["away_ps"]
+    # amount = request.form["amount"]
+    # print game_key, over_under, home_team, home_ml, home_ps, away_team, away_ml, away_ps, amount
+        # return redirect(url_for('nfl_confirm_bet'))
     nfl_game = [d for d in schedule if d['GameKey'] == game_key][0]
     return render_template(
         "nfl_create_bet.html",
@@ -81,8 +94,9 @@ def nfl_confirm_bet():
     away_ml = request.form["away_ml"]
     away_ps = request.form["away_ps"]
     amount = request.form["amount"]
-    print game_key, over_under, home_team, home_ml, home_ps, away_team, away_ml, away_ps, amount
-    return "nfl confim"
+    lst = [game_key, over_under, home_team, home_ml, home_ps, away_team, away_ml, away_ps, amount]
+    print [entry for entry in lst if entry != ""]
+    return render_template("nfl_confirm.html")
 
 @nfl_blueprint.route("/nfl/scores/")
 def nfl_scores():
