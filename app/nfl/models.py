58b64c93a31d1d3bee9750dc22307ee680eea592
Jeff Reiher <jreiher2003@yahoo.com>
2016-09-20 12:27:24 -0400

    Add Create NFL bet model

---
+++ app/nfl/models.py
@@ -0,0 +1,25 @@
+import datetime
+from app import db
+from app.users.models import Users, Role, UserRoles, Profile
+
+class NflBet(db.Model):
+    __tablename__ = "nflbet"
+
+    id = db.Column(db.Integer, primary_key=True)
+    game_key = db.Column(db.Integer)
+    over_under = db.Column(db.Integer)
+    home_team = db.Column(db.String(25))
+    home_ml = db.Column(db.Integer)
+    home_ps = db.Column(db.Integer)
+    away_team = db.Column(db.String(25))
+    away_ml = db.Column(db.Integer)
+    away_ps = db.Column(db.Integer)
+    amount = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey(Users.id))
+    users = db.relationship(Users)
+    date_created = db.Column(db.DateTime(),  default=datetime.datetime.now())
+    date_modified = db.Column(db.DateTime,  default=datetime.datetime.now(),
+                                       onupdate=datetime.datetime.now())
+
+    def __repr__(self):
+        return "<gamekey-{}".format(self.game_key)
