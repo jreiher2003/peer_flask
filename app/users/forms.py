763a2b2c74a96bead1e4546fa06c88b80fd3759f
Jeff Reiher <jreiher2003@yahoo.com>
2016-09-22 13:15:51 -0400

    Add jquery front validation to flask-security

--- app/users/forms.py
+++ app/users/forms.py
@@ -1,5 +1,11 @@
-from flask_security.forms import RegisterForm, Required
+from flask_security.forms import RegisterForm, Required, ValidationError
 from wtforms import TextField
+from .models import Users 
+
+def validate_username(form, field):
+    username = Users.query.filter(Users.username == field.data).first()
+    if username is not None:
+        raise ValidationError("A user with that username already exists")
 
 class ExtendedConfirmRegisterForm(RegisterForm):
-    username = TextField("Username", [Required()])
+    username = TextField("Username", [Required(), validate_username])
