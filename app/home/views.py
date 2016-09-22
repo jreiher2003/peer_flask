7452625bbe7831a11d81bd4c3e0632d17def1a3a
Jeff Reiher <jreiher2003@yahoo.com>
2016-09-22 01:22:12 -0400

    Add some front end validation code

--- app/home/views.py
+++ app/home/views.py
@@ -8,7 +8,7 @@ from flask import Blueprint, render_template
 
 home_blueprint = Blueprint("home", __name__, template_folder="templates")
 
-@home_blueprint.route("/")
+@home_blueprint.route("/", methods=["GET","POST"])
 def home():
     return render_template("home.html")
 
