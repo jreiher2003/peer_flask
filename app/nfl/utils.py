50a3149289c4d0a227ce229045b67650c287f2d0
Jeff Reiher <jreiher2003@yahoo.com>
2016-09-26 17:57:45 -0400

    Refactor nfl_team_home view function to use db querys

--- app/nfl/utils.py
+++ app/nfl/utils.py
@@ -2,17 +2,8 @@ import json
 import datetime
 import random
 from string import hexdigits
-
-with open('sports/Schedule.2016.json') as data_file:    
-        data = json.load(data_file)
-with open('sports/Standing.2016.json') as data_file1:    
-        standing = json.load(data_file1)
-with open('sports/Team.2016.json') as data_file2:    
-        nflteam = json.load(data_file2)
-with open('sports/Stadium.2016.json') as data_file3:    
-        stadium = json.load(data_file3)
-with open('sports/TeamSeason.2016.json') as data_file4:    
-        teamseason = json.load(data_file4)
+from app import db
+from app.nfl_stats.models import NFLStandings, NFLTeam, NFLStadium, NFLSchedule, NFLScore, NFLTeamSeason
 
 def make_salt(length=10):
     return "".join(random.choice(hexdigits) for x in xrange(length))
@@ -30,61 +21,51 @@ def yesterday():
 
 ## OFFENSIVE STATS by TEAM
 def team_rush_avg(rush_yds, team):
-    team_rush_avg = []
-    for x in teamseason:
-        if x["SeasonType"] == 1:
-            team_rush_avg.append((x["RushingYards"],x["Team"]))
-    team_rush_avg =  sorted(team_rush_avg, key=lambda x: x[0], reverse=True) 
-    rank = team_rush_avg.index((rush_yds, team)) + 1
-    return rank
+    q = db.session.query(NFLTeamSeason.RushingYards,NFLTeamSeason.Team).filter_by(SeasonType=1).order_by('RushingYards desc').all()
+    team_ = []
+    for y,t in q:
+        team_.append((y,t)) 
+    return team_.index((rush_yds, team)) + 1
+    
 
 def team_pass_avg(pass_yds, team):
-    team_pass_avg = []
-    for x in teamseason:
-        if x["SeasonType"] == 1:
-            team_pass_avg.append((x["PassingYards"],x["Team"]))
-    team_pass_avg =  sorted(team_pass_avg, key=lambda x: x[0], reverse=True) 
-    rank = team_pass_avg.index((pass_yds, team)) + 1
-    return rank
+    q = db.session.query(NFLTeamSeason.PassingYards,NFLTeamSeason.Team).filter_by(SeasonType=1).order_by('PassingYards desc').all()
+    team_ = []
+    for y,t in q:
+        team_.append((y,t))
+    return team_.index((pass_yds, team)) + 1
+    
 
 def opp_team_rush_avg(rush_yds, team):
-    oppteam_rush_avg = []
-    for x in teamseason:
-        if x["SeasonType"] == 1:
-            oppteam_rush_avg.append((x["OpponentRushingYards"],x["Team"]))
-    oppteam_rush_avg =  sorted(oppteam_rush_avg, key=lambda x: x[0]) 
-    rank = oppteam_rush_avg.index((rush_yds, team)) + 1
-    return rank
+    q = db.session.query(NFLTeamSeason.OpponentRushingYards,NFLTeamSeason.Team).filter_by(SeasonType=1).order_by('OpponentRushingYards desc').all()
+    team_ = []
+    for y,t in q:
+        team_.append((y,t))
+    return team_.index((rush_yds, team)) + 1
 
 def opp_team_pass_avg(pass_yds, team):
-    oppteam_pass_avg = []
-    for x in teamseason:
-        if x["SeasonType"] == 1:
-            oppteam_pass_avg.append((x["OpponentPassingYards"],x["Team"]))
-    oppteam_pass_avg =  sorted(oppteam_pass_avg, key=lambda x: x[0]) 
-    rank = oppteam_pass_avg.index((pass_yds, team)) + 1
-    return rank
+    q = db.session.query(NFLTeamSeason.OpponentPassingYards,NFLTeamSeason.Team).filter_by(SeasonType=1).order_by('OpponentPassingYards desc').all()
+    team_ = []
+    for y,t in q:
+        team_.append((y,t))
+    return team_.index((pass_yds, team)) + 1
 
 def team_off_avg(pass_yds, team):
-    team_pass_avg = []
-    for x in teamseason:
-        if x["SeasonType"] == 1:
-            team_pass_avg.append((x["OffensiveYards"],x["Team"]))
-    team_pass_avg =  sorted(team_pass_avg, key=lambda x: x[0], reverse=True) 
-    rank = team_pass_avg.index((pass_yds, team)) + 1
-    return rank
+    q = db.session.query(NFLTeamSeason.OffensiveYards,NFLTeamSeason.Team).filter_by(SeasonType=1).order_by('OffensiveYards desc').all()
+    team_ = []
+    for y,t in q:
+        team_.append((y,t))
+    return team_.index((pass_yds, team)) + 1
 
 def team_def_avg(pass_yds, team):
-    team_pass_avg = []
-    for x in teamseason:
-        if x["SeasonType"] == 1:
-            team_pass_avg.append((x["OpponentOffensiveYards"],x["Team"]))
-    team_pass_avg =  sorted(team_pass_avg, key=lambda x: x[0]) 
-    rank = team_pass_avg.index((pass_yds, team)) + 1
-    return rank
+    q = db.session.query(NFLTeamSeason.OpponentOffensiveYards,NFLTeamSeason.Team).filter_by(SeasonType=1).order_by('OpponentOffensiveYards desc').all()
+    team_ = []
+    for y,t in q:
+        team_.append((y,t))
+    return team_.index((pass_yds, team)) + 1
 
 # NFL OFFENSIVE STATS ##################################################
-def nfl_off_yds(col_to_sort):
-    return sorted(teamseason, key=lambda k: int(k[col_to_sort]), reverse=True)
+# def nfl_off_yds(col_to_sort):
+#     return sorted(teamseason, key=lambda k: int(k[col_to_sort]), reverse=True)
     
              
--- app/nfl/views.py
+++ app/nfl/views.py
@@ -6,14 +6,14 @@ from dateutil.parser import parse as parse_date
 from app import app, db
 from app.users.models import Users, Role, UserRoles, Profile
 from .models import OverUnderBet, HomeTeamBet, AwayTeamBet
-from app.nfl_stats.models import NFLStandings, NFLTeam, NFLStadium, NFLSchedule, NFLScore
+from app.nfl_stats.models import NFLStandings, NFLTeam, NFLStadium, NFLSchedule, NFLScore, NFLTeamSeason
 from forms import OverUnderForm, HomeTeamForm, AwayTeamForm
 from flask import Blueprint, render_template, url_for, request, redirect,flash, abort
 from flask_security import login_required, roles_required, current_user
 from slugify import slugify
 from .utils import team_rush_avg, team_pass_avg, \
 opp_team_rush_avg, opp_team_pass_avg, team_off_avg, \
-team_def_avg, today_date,today_and_now, nfl_off_yds, make_salt, yesterday
+team_def_avg, today_date,today_and_now, make_salt, yesterday
 
 
 nfl_blueprint = Blueprint("nfl", __name__, template_folder="templates")
@@ -223,48 +223,38 @@ def nfl_stats(sid):
     teamseason1 = [x for x in teamseason if x["SeasonType"] == sid]
     return render_template("nfl_stats.html", all_teams=all_teams, teamseason=teamseason1,)
 
-@nfl_blueprint.route("/nfl/team/home/<int:sid>/<path:team>/")
-def nfl_team_home(sid,team):
+@nfl_blueprint.route("/nfl/team/home/<int:sid>/<path:key>/<path:team>/")
+def nfl_team_home(sid,key,team):
     all_teams = all_nfl_teams()
     dt = today_date()
-    nfl_team = [x for x in nflteam if slugify(x["FullName"]) == team]
-    nfl_team_key = [x["Key"] for x in nfl_team][0]
-    team_season_stats = [x for x in teamseason if x["Team"] == nfl_team_key]
-    team_season_stats1 = [x for x in team_season_stats if x["SeasonType"] == sid]
-
-    team_schedule1 = [x for x in schedule if x["AwayTeam"] == nfl_team_key or x["HomeTeam"] == nfl_team_key]
-    team_schedule = [x for x in team_schedule1 if x["SeasonType"] == sid]
-    team_score1 = [x for x in score if x["SeasonType"] == sid]
-    team_score = [x for x in team_score1 if x["AwayTeam"] == nfl_team_key or x["HomeTeam"] == nfl_team_key]
-
-    team_rush_rank = [team_rush_avg(x["RushingYards"],x["Team"]) for x in team_season_stats if x["SeasonType"] == 1][0]
-    team_pass_rank = [team_pass_avg(x["PassingYards"],x["Team"]) for x in team_season_stats if x["SeasonType"] == 1][0]
-    opp_team_rush_rank = [opp_team_rush_avg(x["OpponentRushingYards"],x["Team"]) for x in team_season_stats if x["SeasonType"] == 1][0]
-    opp_team_pass_rank = [opp_team_pass_avg(x["OpponentPassingYards"],x["Team"]) for x in team_season_stats if x["SeasonType"] == 1][0]
-    team_off_rank = [team_off_avg(x["OffensiveYards"],x["Team"]) for x in team_season_stats if x["SeasonType"] == 1][0]
-    team_def_rank = [team_def_avg(x["OpponentOffensiveYards"],x["Team"]) for x in team_season_stats if x["SeasonType"] == 1][0]
-
-    nflfull = [x["FullName"] for x in nfl_team][0]
-    sID = [x["StadiumID"] for x in nfl_team][0]
-    team_stadium = [y for y in stadium if y["StadiumID"] == sID]
-    team_standing = [z for z in standing if z["Name"] == nflfull]
+    jj = NFLTeam.query.filter_by(Key=key).one()
+    tt = NFLStadium.query.filter_by(StadiumID=jj.StadiumID).one() 
+    ss = NFLStandings.query.filter_by(Team=key).one()
+    tss = NFLTeamSeason.query.filter_by(Team=key, SeasonType=1).one()
+    ts = NFLSchedule.query.filter_by(SeasonType=sid).filter((NFLSchedule.AwayTeam==key) | (NFLSchedule.HomeTeam==key))
+    team_score = NFLScore.query.filter_by(SeasonType=sid).filter((NFLScore.AwayTeam==key) | (NFLScore.HomeTeam==key))
+    team_rush_rank = team_rush_avg(tss.RushingYards,tss.Team) 
+    team_pass_rank = team_pass_avg(tss.PassingYards,tss.Team) 
+    opp_team_rush_rank = opp_team_rush_avg(tss.OpponentRushingYards,tss.Team) 
+    opp_team_pass_rank = opp_team_pass_avg(tss.OpponentPassingYards,tss.Team) 
+    team_off_rank = team_off_avg(tss.OffensiveYards,tss.Team)
+    team_def_rank = team_def_avg(tss.OpponentOffensiveYards,tss.Team) 
     return render_template(
         "nfl_team_home.html",
-        nfl_team=nfl_team,
-        nfl_stadium_standing=zip(nfl_team,team_stadium,team_standing),
         all_teams=all_teams,
-        team_season_stats=team_season_stats1,
         team_rush_rank=team_rush_rank,
         team_pass_rank=team_pass_rank,
         opp_team_rush_rank=opp_team_rush_rank,
         opp_team_pass_rank=opp_team_pass_rank,
         team_off_rank=team_off_rank,
         team_def_rank=team_def_rank,
-        team_schedule=team_schedule,
         team_score=team_score,
-        team_schedule_team_score=zip(team_schedule,team_score),
         dt=dt,
-        team=team
+        tt=tt,
+        jj=jj,
+        ss=ss,
+        tss=tss,
+        ts=ts
         )
 
 @nfl_blueprint.route("/nfl/team/schedule/<path:team>/")
