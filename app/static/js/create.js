f47e843651d9f69877afc8c336445a3b47cc212a
Jeff Reiher <jreiher2003@yahoo.com>
2016-09-27 15:18:02 -0400

    Uncomment js to contorl bet-tabs

--- app/static/js/create.js
+++ app/static/js/create.js
@@ -1,19 +1,19 @@
-// $('#bet_tabs').on('click', 'a[data-toggle="tab"]', function(e) {
-//       e.preventDefault();
-//       var $link = $(this);
-//       if (!$link.parent().hasClass('active')) {
-//         // console.log($link);
-//         //remove active class from other tab-panes
-//         $('.tab-content:not(.' + $link.attr('href').replace('#','') + ') .tab-pane').removeClass('active');
-//         // console.log($('.tab-content:not(.' + $link.attr('href').replace('#','') + ') .tab-pane').removeClass('active'));
-//         // click first submenu tab for active section
-//         $('a[href="' + $link.attr('href') + '_all"][data-toggle="tab"]').click();
-//         // console.log($('a[href="' + $link.attr('href') + '_all"][data-toggle="tab"]'));
-//         // activate tab-pane for active section
-//         $('.tab-content.' + $link.attr('href').replace('#','') + ' .tab-pane:first').addClass('active');
-//         // console.log($('.tab-content.' + $link.attr('href').replace('#','') + ' .tab-pane:first').addClass('active'));
-//       }
-// });
+$('#bet_tabs').on('click', 'a[data-toggle="tab"]', function(e) {
+      e.preventDefault();
+      var $link = $(this);
+      if (!$link.parent().hasClass('active')) {
+        // console.log($link);
+        //remove active class from other tab-panes
+        $('.tab-content:not(.' + $link.attr('href').replace('#','') + ') .tab-pane').removeClass('active');
+        // console.log($('.tab-content:not(.' + $link.attr('href').replace('#','') + ') .tab-pane').removeClass('active'));
+        // click first submenu tab for active section
+        $('a[href="' + $link.attr('href') + '_all"][data-toggle="tab"]').click();
+        // console.log($('a[href="' + $link.attr('href') + '_all"][data-toggle="tab"]'));
+        // activate tab-pane for active section
+        $('.tab-content.' + $link.attr('href').replace('#','') + ' .tab-pane:first').addClass('active');
+        // console.log($('.tab-content.' + $link.attr('href').replace('#','') + ' .tab-pane:first').addClass('active'));
+      }
+});
 // function makeid()
 // {
 //     var text = "";
