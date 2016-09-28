dd2768e7c7a700be093617e1f767840a43bada12
Jeff Reiher <jreiher2003@yahoo.com>
2016-09-27 21:26:54 -0400

    Add js to boostrap to store tab link as active on refresh of page

--- app/static/js/nfl_team_content_tabs.js
+++ app/static/js/nfl_team_content_tabs.js
@@ -1,4 +1,20 @@
-// $("ul.nav-tabs a").click(function (e) {
-//   e.preventDefault();  
-//     $(this).tab('show');
-// });
+$(document).ready(function(){
+    $("#bet_tabs a").click(function (e) {
+      e.preventDefault();  
+        $(this).tab('show');
+    });
+
+    // store the currently selected tab in the hash value
+    $("ul.nav-tabs > li > a").on("shown.bs.tab", function(e) {
+      var id = $(e.target).attr("href").substr(1);
+      console.log("id " + id);
+      window.location.hash = id;
+    });
+
+    // on load of the page: switch to the currently selected tab
+    var hash = window.location.hash;
+    console.log("hash " +hash)
+    $('#bet_tabs a[href="' + hash + '"]').tab('show');
+
+});
+
