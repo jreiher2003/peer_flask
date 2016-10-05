b525c6d23d5ac8ca2a64d06ddd382b7db943350f
Jeff Reiher <jreiher2003@yahoo.com>
2016-10-05 00:19:11 -0400

    Add no change to profile view function

--- app/home/views.py
+++ app/home/views.py
@@ -23,7 +23,7 @@ def home():
 def profile():
     all_teams = all_nfl_teams()
     user = Users.query.filter_by(id=current_user.id).one()
-    pending_bets = NFLcreateBet.query.filter((NFLcreateBet.user_id==user.id) | (NFLcreateBet.taken_by==user.id)).filter_by(bet_taken=True,bet_graded=False).all()
+    pending_bets = NFLcreateBet.query.filter((NFLcreateBet.user_id==user.id) | (NFLcreateBet.taken_by==user.id)).filter_by(bet_taken=True, bet_graded=False).all()
     return render_template("profile.html", user=user, pending_bets=pending_bets, all_teams=all_teams)
 
 @home_blueprint.route("/admin/")
