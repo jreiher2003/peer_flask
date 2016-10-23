bb6e2e34d154329d99b95acec51447d012160464
Jeff Reiher <jreiher2003@yahoo.com>
2016-10-23 00:52:41 +0000

    Update admin model

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt 
 from flask_login import LoginManager 
 from flask_security import Security, SQLAlchemyUserDatastore
+from flask_admin import Admin 
+from flask_admin.contrib.sqla import ModelView
 from block_io import BlockIo
 
 app = Flask(__name__) 
@@ -19,6 +21,8 @@ manager = Manager(app)
 version = 2
 block_io = BlockIo("ef62-6d12-8127-566c", "finn7797", version)
 login_manager = LoginManager(app) 
+
+admin = Admin(app, name="Peer 2 Peer", template_mode="bootstrap3")
  
 
 from app.users.views import users_blueprint
@@ -27,6 +31,8 @@ from app.home.views import home_blueprint
 app.register_blueprint(home_blueprint) 
 from app.nfl.views import nfl_blueprint
 app.register_blueprint(nfl_blueprint) 
+# from app.admin.views import admin_blueprint
+# app.register_blueprint(admin_blueprint)
 
 from temp_filters import dateify, datetimefilter, urlify, datetimefilter_f, game_time, game_date, game_day
 app.jinja_env.filters["dateify"] = dateify
@@ -37,9 +43,26 @@ app.jinja_env.filters["game_time"] = game_time
 app.jinja_env.filters["game_date"] = game_date
 app.jinja_env.filters["game_day"] = game_day
 
-from app.users.models import Users, Role, UserRoles, Profile
-user_daatastore = SQLAlchemyUserDatastore(db, Users, Role)
-security = Security(app, user_daatastore)
+from app.users.models import Users, Role, UserRoles, Profile, BitcoinWallet
+from app.nfl.models import Base, NFLOverUnderBet, NFLSideBet, NFLMLBet, NFLBetGraded
+user_datastore = SQLAlchemyUserDatastore(db, Users, Role)
+security = Security(app, user_datastore)
+
+# ModelView(model, session, name=None, category=None, endpoint=None, url=None, static_folder=None, menu_class_name=None, menu_icon_type=None, menu_icon_value=None)
+# from app.admin.models import UserAdmin
+# admin.add_view(UserAdmin(Users, db.session))
+
+admin.add_view(ModelView(Users, db.session, endpoint="users_"))
+admin.add_view(ModelView(Profile, db.session))
+admin.add_view(ModelView(BitcoinWallet, db.session))
+admin.add_view(ModelView(Role, db.session))
+admin.add_view(ModelView(UserRoles, db.session))
+admin.add_view(ModelView(Base, db.session))
+admin.add_view(ModelView(NFLOverUnderBet, db.session, name="NFL OU"))
+admin.add_view(ModelView(NFLSideBet, db.session, name="NFL SB"))
+admin.add_view(ModelView(NFLMLBet, db.session, name="NFL ML"))
+admin.add_view(ModelView(NFLBetGraded, db.session, name="nflGraded"))
+
 
 login_manager.login_view = "users.login"
 login_manager.login_message = "You need to login first before you can continue."
