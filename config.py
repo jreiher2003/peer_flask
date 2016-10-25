6b3ede5ac929f97a51a316c22aa508d85561ff28
Jeff Reiher <jreiher2003@yahoo.com>
2016-10-25 23:58:02 +0000

    Update configs

--- config.py
+++ config.py
@@ -8,12 +8,12 @@ class BaseConfig(object):
     SQLALCHEMY_DATABASE_URI = "sqlite:///peer.db"
     CACHE_TYPE = "memcached"
     BCRYPT_LOG_ROUNDS = 12
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
+    MAIL_SERVER = os.environ["MAIL_SERVER"]
+    MAIL_PORT = os.environ["MAIL_PORT"]
     MAIL_USE_SSL = True
     MAIL_USE_TLS = False
-    MAIL_USERNAME = "jeffreiher@gmail.com"
-    MAIL_PASSWORD = "7797finn"
+    MAIL_USERNAME = os.environ["MAIL_USERNAME"]
+    MAIL_PASSWORD = os.environ["MAIL_PASSWORD"]
     MAIL_DEFAULT_SENDER = '"Site Admin" <noreply@peer2peer.com>'
 
     SECURITY_UNAUTHORIZED_VIEW = "/nfl/"
