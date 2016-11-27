20358e6868edc5ffa345d1309722b50e2a005cee
Jeff Reiher <jreiher2003@yahoo.com>
2016-11-27 15:39:24 -0500

    Fix bug within action view

--- app/nfl/views.py
+++ app/nfl/views.py
@@ -359,30 +359,30 @@ def nfl_bet_vs_bet(bet_key):
     if form.validate_on_submit():
         print bc_amount, type(bc_amount)
         print bt_amount, type(bt_amount)
-        #form validate on submit
-        network_fees = block_io.get_network_fee_estimate(amounts = (nfl.amount), from_addresses = (bc), to_addresses = (default), priority="low")
-        network_fees = float(network_fees["data"]["estimated_network_fee"])
-        taken_network_fees = block_io.get_network_fee_estimate(amounts = (nfl.amount), from_addresses = (bt), to_addresses = (default), priority="low")
-        taken_network_fees = float(taken_network_fees["data"]["estimated_network_fee"])
-        print network_fees, type(network_fees)
         nflamount = float(nfl.amount)
-        if float(bet_taker.bitcoin_wallet.available_btc) >= (nflamount+network_fees) and float(bet_creator.bitcoin_wallet.available_btc) >= (nflamount+network_fees):
-            print nflamount, type(nflamount),network_fees,type(network_fees)
-            print nflamount + network_fees
-            # check again if balances are enough to cover bets 
-            nfl.bet_taken = True
-            nfl.taken_by = current_user.id 
-            nfl.taken_username = current_user.username 
-            nfl.network_fees = Decimal(network_fees)
-            nfl.taken_network_fees = Decimal(taken_network_fees)
-            bet_creator.profile.bets_taken += 1
-            bet_creator.profile.pending += Decimal(nflamount)
-            bet_taker.profile.bets_taken += 1
-            bet_taker.profile.pending += Decimal(nflamount)
-            bc = bet_creator.bitcoin_wallet.address
-            bt = bet_taker.bitcoin_wallet.address
-        #     # we have to check to see if profile_taker and profile_bet_creator have enough bitcoins in their account to proceed.
+        if float(bet_taker.bitcoin_wallet.available_btc) >= (nflamount+.0002) and float(bet_creator.bitcoin_wallet.available_btc) >= (nflamount+.0002):
+              # check again if balances are enough to cover bets 
+              # we have to check to see if profile_taker and profile_bet_creator have enough bitcoins in their account to proceed.
             try:
+                #form validate on submit
+                network_fees = block_io.get_network_fee_estimate(amounts = (nfl.amount), from_addresses = (bc), to_addresses = (default), priority="low")
+                network_fees = float(network_fees["data"]["estimated_network_fee"])
+                taken_network_fees = block_io.get_network_fee_estimate(amounts = (nfl.amount), from_addresses = (bt), to_addresses = (default), priority="low")
+                taken_network_fees = float(taken_network_fees["data"]["estimated_network_fee"])
+                # print network_fees, type(network_fees)
+                # print nflamount, type(nflamount),network_fees,type(network_fees)
+                # print nflamount + network_fees
+                nfl.bet_taken = True
+                nfl.taken_by = current_user.id 
+                nfl.taken_username = current_user.username 
+                nfl.network_fees = Decimal(network_fees)
+                nfl.taken_network_fees = Decimal(taken_network_fees)
+                bet_creator.profile.bets_taken += 1
+                bet_creator.profile.pending += Decimal(nflamount)
+                bet_taker.profile.bets_taken += 1
+                bet_taker.profile.pending += Decimal(nflamount)
+                bc = bet_creator.bitcoin_wallet.address
+                bt = bet_taker.bitcoin_wallet.address
                 block_io.withdraw_from_addresses(amounts = bc_amount, from_addresses = bc, to_addresses = default, priority="low", nonce=nonce)
                 block_io.withdraw_from_addresses(amounts = bt_amount, from_addresses = bt, to_addresses = default, priority="low", nonce=nonce1)
                 db.session.add_all([bet_taker, bet_creator, nfl])
@@ -393,7 +393,7 @@ def nfl_bet_vs_bet(bet_key):
             except Exception:
                 print sys.exc_info()[1],"payment error"
         else:
-            flash("You or your opponent don't have enough money in their account to make this bet.  This is for your protection.", "danger")
+            flash("You or your opponent don't have enough money in their account to make this bet.  This is for your protection. Please check your balance/pending and try again!", "danger")
             return redirect(url_for('nfl.nfl_edit_bet', bet_key=bet_key))
     return render_template(
         "nfl_vs_bet.html", 
