e0883975900bb44e684ed6511c7f479d4dd31cd5
Jeff Reiher <jreiher2003@yahoo.com>
2016-12-04 13:37:20 -0500

    Update

--- app/users/models.py
+++ app/users/models.py
@@ -11,7 +11,7 @@ class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True)#nullable=False, 
     email = db.Column(db.String(255), nullable=False, unique=True)
-    _password = db.Column(db.Binary(255), nullable=False) #hybrid column
+    _password = db.Column(db.String(255), nullable=False) #hybrid column
     confirmed = db.Column(db.Boolean(), default=False)
     confirmed_at = db.Column(db.DateTime)
     date_created = db.Column(db.DateTime,  default=datetime.datetime.utcnow)
